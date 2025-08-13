cbf0defd592a57531682954858b27039aafeb267
jaehyeokjin <jhjin@uchicago.edu>
2025-08-13 19:39:10 -0400

    Update RDF

---
+++ Results/Structure/RDF/AA/state/ucgrun.sh
@@ -0,0 +1,3 @@
+nohup ./ucg_rdf.x ../../step2/short.lammpstrj ./aa_11.out L L &
+nohup ./ucg_rdf.x ../../step2/short.lammpstrj ./aa_12.out D L &
+nohup ./ucg_rdf.x ../../step2/short.lammpstrj ./aa_22.out D D &
---
+++ Results/Structure/RDF/UCG/ucgrun.sh
@@ -0,0 +1,3 @@
+nohup ./ucg_rdf.x ../mscg_nvt.lammpstrj ./ucg_11.out L L &
+nohup ./ucg_rdf.x ../mscg_nvt.lammpstrj ./ucg_12.out D L &
+nohup ./ucg_rdf.x ../mscg_nvt.lammpstrj ./ucg_22.out D D &
